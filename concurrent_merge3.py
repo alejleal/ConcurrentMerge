e0e32b4083479963ff74b2f9dd02ab4837e1cbfb
alejleal <alejleal@ucm.es>
2022-03-04 18:18:25 +0100

    Añadidos comentarios y ligeras modificaciones

--- concurrent_merge3.py
+++ concurrent_merge3.py
@@ -1,100 +1,69 @@
-from multiprocessing import BoundedSemaphore, Process, Lock, Value, Array, Semaphore
+from multiprocessing import BoundedSemaphore, Process, Lock, Array, Semaphore
 from random import randint
-
-from sympy import false
 
 NPROD = 10
 MAXPROD = 200
 
-def producer(pid, buffer, empty, non_empty, mutex):
+def producer(pid, buffer, empty, non_empty):
     last = 0
     for _ in range(MAXPROD):
         empty[pid].acquire()
         
-        mutex.acquire()            
         last += randint(0, 5)
         buffer[pid] = last
-        mutex.release()
 
         non_empty[pid].release()
 
     empty[pid].acquire()
-    mutex.acquire()            
     buffer[pid] = -1
-    mutex.release()
     non_empty[pid].release()    
 
 
-def merger(buffer, empty, non_empty, mutex, sorted_list):
+def merger(buffer, empty, non_empty, sorted_list):
     for s in non_empty:
-        s.acquire()
+        s.acquire()                                 # Espera al principio a que todos produzcan
     
-    # running = [True]*NPROD
-
     min_index = mindex(buffer)
-    fin = min_index == -1
+    # fin = min_index == -1
 
-    while not fin:
-        mutex.acquire()
+    while max(buffer) > -1:                        # Como los numeros generados son positivos, si el maximo es -1 entonces significa que todos han acabado
         sorted_list.append(buffer[min_index])
-        mutex.release()
 
         empty[min_index].release()
         non_empty[min_index].acquire()
 
-        mutex.acquire()
-        # if buffer[min_index] == -1:
-            # running[min_index] = False
         min_index = mindex(buffer)
-        fin = min_index == -1
-        mutex.release()
+        # fin = min_index == -1                       # Comprueba si todos han terminado
 
-    print(sorted_list[:-1], len(sorted_list))
+    print(sorted_list, len(sorted_list))
 
 def mindex(buffer):
-    pos_buffer = [e for e in buffer if e >= 0]
-    if not pos_buffer:
+    pos_buffer = [e for e in buffer if e >= 0]      # Filtra los positivos
+    if not pos_buffer:                              # Si no hay postivos devuelve -1
         return -1
-    l = list(buffer)            # Array no tiene un metodo llamado index 
+    l = list(buffer)                                # Array no tiene un metodo llamado index 
     return l.index(min(pos_buffer))
-
-    l = len(buffer)
-    if l == 0 or l == 1:
-        return l - 1
-
-    if max(buffer) < 0:
-        return -1
-
-    min_index = 0
-    min_buffer = buffer[0]
-
-    for i in range(1, l):
-        if buffer[i] >= 0 and buffer[0] < min_buffer:
-            min_index = i
-            min_buffer = buffer[i]
-    
-    return min_index
 
 def main():
     buffer = Array('i', [0]*NPROD)
     empty = []
     non_empty = []
-    mutex = Lock()
 
-    for _ in range(NPROD):
-        empty.append(BoundedSemaphore(1))
-        non_empty.append(Semaphore(0))
+    empty = [BoundedSemaphore(1) for _ in range(NPROD)]
+    non_empty = [Semaphore(0) for _ in range(NPROD)]
 
     sorted_list = []
 
-    lp = [Process(target=producer, args=(pid, buffer, empty, non_empty, mutex)) for pid in range(NPROD)]
-    lp.append(Process(target=merger, args=(buffer, empty, non_empty, mutex, sorted_list)))
+    lp = [Process(target=producer, args=(pid, buffer, empty, non_empty)) for pid in range(NPROD)]
+    lp.append(Process(target=merger, args=(buffer, empty, non_empty, sorted_list)))
 
     for p in lp:
         p.start()
 
     for p in lp:
         p.join()
+
+    print(sorted_list)
 
 if __name__ == "__main__":
     main()
--- concurrent_merge4.py
+++ concurrent_merge4.py
@@ -1,83 +1,76 @@
-from multiprocessing import BoundedSemaphore, Process, Lock, Value, Array, Semaphore
+from multiprocessing import BoundedSemaphore, Process, Lock, Array, Semaphore
 from random import randint
+
+"""
+Nota: El almacen de cada productor se simula haciendo que el segmento buffer[i*CAP:(i + 1)*CAP] corresponda al productor i-ésimo.
+Para añadir/consumir elementos de cada almacén se hace de manera modular. Por tanto, cada vez que se llega al final del almacén
+de algun productor, se prosigue generando/consumiendo al inicio del segmento. De esta manera no es necesario mover todos los
+numeros de sitio cada vez que se consume uno de ellos.
+"""
 
 CAP = 7
 NPROD = 10
 MAXPROD = 300
 
-def producer(pid, buffer, empty, non_empty, mutex):
+def producer(pid, buffer, empty, non_empty):
     last = 0
     lastSaved = 0
     for _ in range(MAXPROD):
         empty[pid].acquire()
         
-        mutex.acquire()            
         last += randint(0, 5)
         buffer[lastSaved + pid*CAP] = last
-        mutex.release()
 
         lastSaved = (lastSaved + 1) % CAP
 
         non_empty[pid].release()
 
+    # Cuando acaba de producir todo, va rellenando con -1
     for _ in range(CAP):
         empty[pid].acquire()
-
-        mutex.acquire()            
         buffer[lastSaved + pid*CAP] = -1
-        mutex.release()
-
         non_empty[pid].release()
 
         lastSaved = (lastSaved + 1) % CAP
 
-
-def merger(buffer, empty, non_empty, mutex, sorted_list):
+def merger(buffer, empty, non_empty, sorted_list):
     for s in non_empty:
-        s.acquire()
+        s.acquire()                                 # Espera al principio a que todos produzcan
     
-    lastConsumed = [0]*NPROD
+    lastConsumed = [0]*NPROD    # Guarda la posición del ultimo elemento que ha consumido en el almacen de cada productor
 
-    min_index = mindex([buffer[lastConsumed[i] + i*CAP] for i in range(NPROD)])
-    fin = min_index == -1
+    min_index = mindex([buffer[lastConsumed[i] + i*CAP] for i in range(NPROD)])     # Filtra los elementos del buffer tomando los que toquen de cada almacen de cada productor
 
-    while not fin:
-        mutex.acquire()
+    while max(buffer) > 1:
         sorted_list.append(buffer[lastConsumed[min_index] + min_index*CAP])
-        lastConsumed[min_index] = (lastConsumed[min_index] + 1) % CAP
-        mutex.release()
+        lastConsumed[min_index] = (lastConsumed[min_index] + 1) % CAP               # Actualiza la posicion del ultimo consumido
 
         empty[min_index].release()
         non_empty[min_index].acquire()
 
-        mutex.acquire()
         min_index = mindex([buffer[lastConsumed[i] + i*CAP] for i in range(NPROD)])
-        fin = min_index == -1
-        mutex.release()
 
-    print(sorted_list[:-1], len(sorted_list))
+    print(sorted_list, len(sorted_list))
 
 def mindex(buffer):
-    pos_buffer = [e for e in buffer if e >= 0]
-    if not pos_buffer:
+    pos_buffer = [e for e in buffer if e >= 0]      # Filtra los positivos
+    if not pos_buffer:                              # Si no hay postivos devuelve -1
         return -1
-    l = list(buffer)            # Array no tiene un metodo llamado index 
+    l = list(buffer)                                # Array no tiene un metodo llamado index 
     return l.index(min(pos_buffer))
 
 def main():
     buffer = Array('i', [0]*NPROD*CAP)
     empty = []
     non_empty = []
-    mutex = Lock()
 
-    for _ in range(NPROD):
-        empty.append(BoundedSemaphore(CAP))
-        non_empty.append(Semaphore(0))
+    empty = [BoundedSemaphore(CAP) for _ in range(NPROD)]
+    non_empty = [Semaphore(0) for _ in range(NPROD)]
 
     sorted_list = []
 
-    lp = [Process(target=producer, args=(pid, buffer, empty, non_empty, mutex)) for pid in range(NPROD)]
-    lp.append(Process(target=merger, args=(buffer, empty, non_empty, mutex, sorted_list)))
+    lp = [Process(target=producer, args=(pid, buffer, empty, non_empty)) for pid in range(NPROD)]
+    lp.append(Process(target=merger, args=(buffer, empty, non_empty, sorted_list)))
 
     for p in lp:
         p.start()
